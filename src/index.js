51d5f43f6e409ae5907307015724207f806b02bf
yanesh <yaneshcode@gmail.com>
2019-04-17 23:32:14 -0400

    state

--- src/index.js
+++ src/index.js
@@ -3,27 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: null
+  //   }
+  // }
   render() {
     return (
       <button className="square" 
       onClick={() => {
-        this.setState({value: 'X'});
+        this.props.onClick({value: 'X'});
       }}>
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return ( <Square value={this.state.squares[i]} onClick={() => {
+      this.handleClick(i) }
+    }/>);
   }
 
   render() {
