3a026e25d218ee7ecd6de30ff180166f06615c46
yanesh <yaneshcode@gmail.com>
2019-04-19 17:50:16 -0400

    move display

--- src/index.js
+++ src/index.js
@@ -146,8 +146,8 @@ class Game extends React.Component {
             }} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
